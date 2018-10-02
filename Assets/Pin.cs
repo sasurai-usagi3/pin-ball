7ae7154a3d568d995c1e4070daee5911d291ccad
宇山 拓夢 <sasurai.usagi3@gmail.com>
2018-10-03 08:05:26 +0900

    feature: フリッパーの運動

---
+++ Assets/Pin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour {
+	Rigidbody rigibody;
+
+	// Use this for initialization
+	void Start () {
+		rigibody = GetComponent<Rigidbody> ();
+		//rigibody.AddForce(new Vector3 (0F, 0F, 10F), ForceMode.Impulse);
+		//transform.RotateAround (new Vector3 (5F, 5F, 5F), Vector3.up, 1F);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
