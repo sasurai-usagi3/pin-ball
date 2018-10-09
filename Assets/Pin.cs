e871daa0a8dd318338358203d4a24fd37001259e
宇山 拓夢 <sasurai.usagi3@gmail.com>
2018-10-10 07:55:26 +0900

    feature: キー操作に合わせてフリッパー運動させる

--- Assets/Pin.cs
+++ Assets/Pin.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Pin : MonoBehaviour {
-	Rigidbody rigibody;
+	HingeJoint hingeJoint;
 
 	// Use this for initialization
 	void Start () {
-		rigibody = GetComponent<Rigidbody> ();
+		hingeJoint = GetComponent<HingeJoint> ();
 		//rigibody.AddForce(new Vector3 (0F, 0F, 10F), ForceMode.Impulse);
 		//transform.RotateAround (new Vector3 (5F, 5F, 5F), Vector3.up, 1F);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		JointSpring spring = hingeJoint.spring;
+
+		if (Input.GetKey (KeyCode.L)) {
+			spring.targetPosition = 100F;
+			hingeJoint.spring = spring;
+		} else {
+			spring.targetPosition = 0F;
+			hingeJoint.spring = spring;
+		}
 	}
 }
