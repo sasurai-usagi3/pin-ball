6ca93373ccee74121172614dc82f7152995af83c
宇山 拓夢 <sasurai.usagi3@gmail.com>
2018-12-05 07:59:39 +0900

    feature: 動く壁

---
+++ Assets/MovingWall.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingWall : MonoBehaviour {
+	Rigidbody rigidbody;
+	Vector3 delta;
+
+	// Use this for initialization
+	void Start () {
+		delta = Vector3.left / 50;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x < -1.8 || 1.6 < transform.position.x) {
+			delta = -delta;
+		}
+
+		transform.position += delta;
+	}
+}
